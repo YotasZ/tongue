00bf736bf863393c45d34a4994afcd421a2a3280
YuanZhouGit <44307914+YuanZhouGit@users.noreply.github.com>
2018-10-30 20:54:04 +0800

    图像处理操作简便化
    原文件只是对图片分割的算法代码部分，但是不能直接锁定某一点的坐标值，本次更改将对图像处理操作面向对象化，重点添加在图片显示后能点击图像前景边框上的几个点，并自动获取前景矩形的信息。但是该算法仍存在以上问题，切割效率不高，切割对应的图片大小不能过大，目前实验超过1MB的图片会显示显卡内存不足的问题，需要压缩图片的操作

--- grabcut.py
+++ grabcut.py
@@ -1,15 +1,69 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
-img = cv2.imread('view1.jpg')
-mask = np.zeros(img.shape[:2], np.uint8)#掩模
-bgdModel = np.zeros((1, 65), np.float64)
-fgdModel = np.zeros((1, 65), np.float64)
 
-rect = (155, 1, 555, img.shape[0])
-#(461, 820, 3)
-cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 10, cv2.GC_INIT_WITH_RECT)
-mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-img = img * mask2[:, :, np.newaxis]
+#点击1退出图像显示
+class picture_dealing:
+    # 对图像的处理，包括基本数据元素：图片
+    # 基本成员函数：对图片的压缩，对图片的切割，对图片色泽及颜色的判断
+    # 实现对图片的处理效果：
+    # 判断图片的大小，如果图片太大，调用图片的压缩函数实现图片的压缩
+    # 选定图片的前景，可以自行输入，也可以在图片上进行点点标记自动得到图片的坐标
+    # 切割图片
+    # 对切割后图片的部分色泽进行分析
+    def __init__(self, picName):
+        self.img = cv2.imread(picName)
 
-cv2.imwrite('result.jpg',img)
+    def get_info(self):#获取前景的x,y,w,h
+        self.loc=[]
+        self.xloc=[]
+        self.yloc=[]
+        def get_loc(event,x,y,flags,param):
+            if event==cv2.EVENT_LBUTTONDOWN:
+                self.loc.append((x,y))
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image',get_loc)
+        while(1):
+            cv2.imshow('image',self.img)
+            k=cv2.waitKey(10)
+            if k==ord('1'):
+                break
+        cv2.destroyAllWindows()
+        for i in self.loc:
+            self.xloc.append(i[0])
+            self.yloc.append(i[1])
+        self.x_max=max(self.xloc)
+        self.x_min=min(self.xloc)
+        self.y_max=max(self.yloc)
+        self.y_min=min(self.yloc)
+        self.width=self.x_max-self.x_min
+        self.height=self.y_max-self.y_min
+        return
+
+
+
+
+    def grabcut(self):
+        rect = (self.x_min, self.y_min, self.width, self.height)
+        mask = np.zeros(self.img.shape[:2], np.uint8)
+        bgdModel = np.zeros((1, 65), np.float64)
+        fgdModel = np.zeros((1, 65), np.float64)
+        cv2.grabCut(self.img, mask, rect, bgdModel, fgdModel, 10, cv2.GC_INIT_WITH_RECT)
+        mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+        self.cutted_pic = self.img * mask2[:, :, np.newaxis]
+
+    def show(self):
+        while (1):
+            cv2.imshow('image', self.cutted_pic)
+            k = cv2.waitKey(10)
+            if k == ord('1'):
+                break
+        cv2.destroyAllWindows()
+
+    def save(self,filename):
+        cv2.imwrite(filename,self.cutted_pic)
+
+pic=picture_dealing('1.jpg')
+pic.get_info()
+pic.grabcut()
+pic.show()
+
